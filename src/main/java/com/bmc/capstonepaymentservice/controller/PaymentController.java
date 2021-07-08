bcf50072f3c7e0a4634576feb211611f7e337c75
abhissaw10 <sawhney.abhishek1983@gmail.com>
2021-07-08 13:07:20 +0530

    Added security configuration

--- src/main/java/com/bmc/capstonepaymentservice/controller/PaymentController.java
+++ src/main/java/com/bmc/capstonepaymentservice/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.bmc.capstonepaymentservice.controller;
 import com.bmc.capstonepaymentservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/payments")
     public ResponseEntity payment(@RequestParam("appointmentId") String appointmentId){
         return ResponseEntity.ok(paymentService.save(appointmentId));
---
+++ src/main/java/com/bmc/capstonepaymentservice/filter/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.bmc.capstonepaymentservice.filter;
+
+import com.bmc.capstonepaymentservice.model.UserPrincipal;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    @Override
+    public UserPrincipal loadUserByUsername(String id) throws UsernameNotFoundException {
+
+        return UserPrincipal.builder().username(id).build();
+    }
+
+
+}
